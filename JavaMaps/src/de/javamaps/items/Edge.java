1e006512a732c9a87620d184746f5870e9e91ad9
Yannjoel <yannj96@googlemail.com>
2016-06-10 11:38:05 +0200

    Erstellung der Typen Knoten und Vertex mit setter und getter methoden

--- JavaMaps/src/de/javamaps/items/Edge.java
+++ JavaMaps/src/de/javamaps/items/Edge.java
@@ -1,13 +1,22 @@
 package de.javamaps.items;
 
 public class Edge{
-	      public final String vertex1;
-	      public final String vertex2;
-	      public final long distance;
+	      private final String vertex1;
+	      private final String vertex2;
+	      private final long distance;
 	      public Edge(String v1, String v2, int distanceIn) {
 	         this.vertex1 = v1;
 	         this.vertex2 = v2;
 	         this.distance = distanceIn;
 	      }
+		public String getVertex1() {
+			return vertex1;
+		}
+		public String getVertex2() {
+			return vertex2;
+		}
+		public long getDistance() {
+			return distance;
+		}
 }
 	
--- JavaMaps/src/de/javamaps/items/Vertex.java
+++ JavaMaps/src/de/javamaps/items/Vertex.java
@@ -5,30 +5,41 @@ import java.util.Map;
 
 
 
-public class Vertex implements Comparable<Vertex> {
-	public final String name;
-	public Vertex previous = null;
-	public int dist = Integer.MAX_VALUE; /// ersatz für unendlich
-	public final Map<Vertex, Integer> adjacentCitys = new HashMap<>();
+public class Vertex {
+	private final String name;
+	private Vertex previous = null;	
+	private int dist = Integer.MAX_VALUE; /// ersatz für unendlich //bisheriger Weg zu diesem Knoten
+	private Map<Vertex, Integer> adjacentVertex = new HashMap<>();
 
 	public Vertex(String name) {
 		this.name = name;
 	}
 
-	public StringBuffer createPath() {
-		StringBuffer forReturn = new StringBuffer(); 
-		if (this == this.previous) {
-			forReturn.append(this.name);
-		} else if (this.previous == null) {
-			forReturn.append("(unreached)" + this.name);
-		} else {
-			forReturn.append(this.previous.createPath());
-			forReturn.append(" -> " + this.name.toString() + "(" + this.dist + ")");
-		}
-		return forReturn;
+	public String getName() {
+		return name;
 	}
 
-	public int compareTo(Vertex other) {
-		return Integer.compare(dist, other.dist);
+	public Vertex getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(Vertex previous) {
+		this.previous = previous;
+	}
+
+	public int getDist() {
+		return dist;
+	}
+
+	public void setDist(int dist) {
+		this.dist = dist;
+	}
+
+	public Map<Vertex, Integer> getAdjacentVertex() {
+		return adjacentVertex;
+	}
+
+	public void setAdjacentVertex(Map<Vertex, Integer> adjacentVertex) {
+		this.adjacentVertex = adjacentVertex;
 	}
 }
