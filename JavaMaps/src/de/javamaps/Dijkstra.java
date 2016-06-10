6b286baec030a67a1b6639bb9d1cceece1700714
Yannjoel <yannj96@googlemail.com>
2016-06-10 12:30:03 +0200

    Datei für Dijkstra erstellt

--- JavaMaps/src/de/javamaps/Dijkstra.java
+++ JavaMaps/src/de/javamaps/Dijkstra.java
@@ -1,5 +1,15 @@
 package de.javamaps;
 
-public class Dijkstra {
+import java.util.ArrayList;
 
+import de.javamaps.items.Edge;
+import de.javamaps.items.Graph;
+import de.javamaps.items.Vertex;
+
+public class Dijkstra {
+	public String getshortestWay(Vertex start, Vertex end, Graph graphMap){
+		StringBuffer output = new StringBuffer();
+		
+		return output.toString();
+	}
 }
