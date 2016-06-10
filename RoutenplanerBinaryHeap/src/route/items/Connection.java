ae46448aa5a5c5c8e4096c36c6d7a35af99eebb0
Yannjoel <yannj96@googlemail.com>
2016-06-10 10:08:20 +0200

    test

---
+++ RoutenplanerBinaryHeap/src/route/items/Connection.java
@@ -0,0 +1,13 @@
+package route.items;
+
+public class Connection{
+	      public String city1;
+	      public String city2;
+	      public final int distance;
+	      public Connection(String c1, String c2, int distanceIn) {
+	         this.city1 = c1;
+	         this.city2 = c2;
+	         this.distance = distanceIn;
+	      }
+}
+	
