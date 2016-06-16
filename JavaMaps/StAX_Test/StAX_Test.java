1213e75edfed1c3286c02dae15118f77f426bba3
Jan-Laptop <Jan-Laptop@172.31.3.156>
2016-06-16 15:07:18 +0200

    ...

--- JavaMaps/StAX_Test/StAX_Test.java
+++ JavaMaps/StAX_Test/StAX_Test.java
@@ -4,28 +4,53 @@ import java.util.*;
 
 public class StAX_Test {
 	public static void main(String [ ] args) throws XMLStreamException, FileNotFoundException, UnsupportedEncodingException{
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 		File file = new File("data/autobahn_bw.kml");
 		InputStreamReader stream = new InputStreamReader(new FileInputStream(file),"ISO-8859-1");
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		XMLStreamReader parser = factory.createXMLStreamReader(stream);
 		
-		List elements = new ArrayList();
+		List<List<String>> elements = new ArrayList<List<String>>();
+		String text;
 		
 		
 		while(parser.hasNext()){
-			if(parser.getEventType() == XMLStreamConstants.START_ELEMENT){
-				System.out.println(parser.getLocalName());
-				if(parser.getLocalName() == "Placemark"){
-					while(parser.getLocalName() != "Placemark"){
-						
+			switch(parser.getEventType()){
+				case XMLStreamReader.START_ELEMENT:
+					if(parser.getLocalName() != "Placemark"){
+						ArrayList<String> singleElement = new ArrayList<String>();
+						singleElement.add(parser.getLocalName());
+						singleElement.add(parser.getAttributeLocalName(0));						
+						singleElement.add(parser.getAttributeValue(0));
+						/*try {
+							text = parser.getElementText();
+				          
+				        } catch (XMLStreamException e) {
+				        	text = null;
+				        }
+						singleElement.add(text);*/
+						elements.add(singleElement);
 					}
-				}
+				break;
+				
+				case XMLStreamReader.END_ELEMENT:
+					if(parser.getLocalName() == "Placemark"){
+						System.out.println("");
+						for(int i = 0; i < elements.size(); i++){
+							System.out.println(elements.get(i).toString());
+						}
+						elements.clear();
+					}
+				break;
+				
+				case XMLStreamReader.END_DOCUMENT:
+					
+				break;
+				
+				default:
+					
+				break;
 			}
-
 			parser.next();
 		}
-		System.out.println("Ende");
-		
 	}
 }
