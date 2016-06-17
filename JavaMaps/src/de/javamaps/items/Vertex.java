c97fae6202d67862995d27ee44ab7f8443718168
Yannjoel <yannj96@googlemail.com>
2016-06-17 10:32:00 +0200

    Umstellung auf neue Erkenntnisse von open Streetmap

--- JavaMaps/src/de/javamaps/items/Vertex.java
+++ JavaMaps/src/de/javamaps/items/Vertex.java
@@ -1,53 +1,30 @@
 package de.javamaps.items;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Vertex {
-	private final int id;
-	private final float Lat;
-	private final float Lon;
+	
+
+	private final int locCode;
+	private final String name;
 	private Vertex previous = null;
 	private int way_dist = Integer.MAX_VALUE; /// ersatz für unendlich
 												/// //bisheriger Weg zu diesem
 												/// Knoten
-	private Map<Vertex, Long> adjacentVertex = new HashMap<>();
-
-	public Vertex(int idIn, float Latitude, float Longitude, Vertex previousVertex, int cumulativ_distance, Map<Vertex, Long> neighbourVertex) {
-		this.id = idIn;
-		this.Lon = Longitude;
-		this.Lat = Latitude;
-		this.previous = previousVertex;
-		this.setWay_dist(cumulativ_distance);
-		this.adjacentVertex = neighbourVertex;
+	private boolean visited;
+	
+	public void setAsStart(){
+		this.way_dist = 0;
 	}
-
-	public int getId() {
-		return id;
+	
+	public Vertex(String NameIn, int locCodeIn) {
+		this.name = NameIn;
+		this.locCode = locCodeIn;
 	}
-
 	public Vertex getPrevious() {
 		return previous;
 	}
 
 	public void setPrevious(Vertex previous) {
 		this.previous = previous;
-	}
-
-	public Map<Vertex, Long> getAdjacentVertex() {
-		return adjacentVertex;
-	}
-
-	public void setAdjacentVertex(Map<Vertex, Long> adjacentVertex) {
-		this.adjacentVertex = adjacentVertex;
-	}
-
-	public float getLon() {
-		return Lon;
-	}
-
-	public float getLat() {
-		return Lat;
 	}
 
 	public int getWay_dist() {
@@ -57,4 +34,23 @@ public class Vertex {
 	public void setWay_dist(int way_dist) {
 		this.way_dist = way_dist;
 	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public int getLocCode() {
+		return locCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
 }
+
+	
