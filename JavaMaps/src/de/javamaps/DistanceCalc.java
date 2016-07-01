a3252e698592349a0c9c711b486abb55158f8c99
TimHeinzelmann <tim-heinzelmann@gmx.net>
2016-07-01 11:35:40 +0200

    Hinzufügen der Dokumentation

--- JavaMaps/src/de/javamaps/DistanceCalc.java
+++ JavaMaps/src/de/javamaps/DistanceCalc.java
@@ -1,13 +1,21 @@
 package de.javamaps;
-
 import de.javamaps.items.*;
 
 import java.util.*;
 import java.util.Map.Entry;
 
+/**
+ * @author Tim
+ * @version 1.01
+ * @since 1.8.0_91
+ */
 public class DistanceCalc {
 	// Genauigkeit: 1m; Returnwert: int;
-	public static HashMap distanceCalculation(HashMap<Long, Vertex> graph) {
+	/**
+	 * @param graph
+	 * @return graph with calculated neighbours for each neighbour
+	 */
+	public static HashMap<Long, Vertex> distanceCalculation(HashMap<Long, Vertex> graph) {
 
 		double distance = 0;
 		for (Entry<Long, Vertex> e : graph.entrySet()) {
