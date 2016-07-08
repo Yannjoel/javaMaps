9f8cdc70e043e5d6dea29fc090d5eed711c65de2
Yannjoel <yannj96@googlemail.com>
2016-07-08 10:20:19 +0200

    Anpassung Junit test an neue Ausgabe Methode

--- JavaMaps/src/de/javamaps/test/DijkstraTest.java
+++ JavaMaps/src/de/javamaps/test/DijkstraTest.java
@@ -18,14 +18,14 @@ public class DijkstraTest {
 	testData.put((long) 1, new Vertex("Stadt1", 1, 0, 0));
 	testData.put((long) 2, new Vertex("Stadt2", 2, 0, 0));
 	testData.put((long) 3, new Vertex("Stadt3", 3, 0, 0));
-	testData.get((long) 1).addNeighbor(new Neighbor(2, 1));
-	testData.get((long) 1).addNeighbor(new Neighbor(3, 5));
-	testData.get((long) 2).addNeighbor(new Neighbor(3, 1));
+	testData.get((long) 1).addNeighbor(new Neighbor(2, 1000));
+	testData.get((long) 1).addNeighbor(new Neighbor(3, 5000));
+	testData.get((long) 2).addNeighbor(new Neighbor(3, 1000));
 	return testData;
 	}
 	@Test
 	public void testAll() {
-		StringBuffer shouldBe = new StringBuffer("Total Distance from Stadt1 to Stadt3 is: 2\nWay was:\n1 (name = Stadt1)\n2 (name = Stadt2)\n3 (name = Stadt3)\n");
+		StringBuffer shouldBe = new StringBuffer("Total Distance from Stadt1 to Stadt3 is: 2.0 km \n \nWay was:\n1 (name = Stadt1)\n2 (name = Stadt2)\n3 (name = Stadt3)\n");
 		StringBuffer is = new StringBuffer(Dijkstra.getshortestWay((long)1,(long) 3,testData()));
 		assertEquals(shouldBe.toString(),is.toString());
 		assertEquals("Cant find 4 in our map\n",Dijkstra.getshortestWay((long)4,(long) 3,testData()).toString());
