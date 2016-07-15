aaac5092014fd702063b520fd07fa8224ab29162
TimHeinzelmann <tim-heinzelmann@gmx.net>
2016-07-15 09:41:38 +0200

    Buildbridge hinzugefügt

---
+++ JavaMaps/src/de/javamaps/BuildBridge.java
@@ -0,0 +1,23 @@
+package de.javamaps;
+
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import de.javamaps.items.*;
+
+public class BuildBridge{
+	public static void createBridge(TreeMap<Long, Vertex> graph){
+		
+		for (Entry<String, List<Long>> e: MotorwayRamp.getMotorwayRamps(graph).entrySet()){
+			for(Long id:e.getValue()){
+				Vertex actual = graph.get(id);
+				for(Long idNewN:e.getValue()){
+					if (idNewN != id){
+						actual.addNeighbor(new Neighbor(idNewN,1));
+					}
+				}
+			}
+
+		}
+	}
+}
