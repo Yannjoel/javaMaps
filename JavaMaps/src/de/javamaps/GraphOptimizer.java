6178bad258faf092ac4af5ecfbc57d9b015e377e
Yannjoel <yannj96@googlemail.com>
2016-08-01 00:02:33 +0200

    Optimierung der Laufzeit

--- JavaMaps/src/de/javamaps/GraphOptimizer.java
+++ JavaMaps/src/de/javamaps/GraphOptimizer.java
@@ -3,50 +3,50 @@ package de.javamaps;
 import java.util.TreeMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Map.Entry;
 import de.javamaps.items.Neighbor;
 import de.javamaps.items.Vertex;
 
 public class GraphOptimizer {
 
-	private static final String STARTMESSAGE =  "Karte wird optimiert - Dies kann einige Minuten dauern";
-	
+	private static final String NOTSET =  "null";
+	private static List<Vertex> visitedVertexes = new LinkedList<Vertex>();
 	public static void uniteVertexs(TreeMap<Long, Vertex> graph) {
-		System.out.println(STARTMESSAGE);
-		TreeMap<Long, Vertex> importantVertexs = new TreeMap<Long, Vertex>();
 		for (Entry<Long, Vertex> entryOfGraph : graph.entrySet()){
-			if (!entryOfGraph.getValue().getName().equals("null") || entryOfGraph.getValue().getNeighbors().size() > 1) {
+			if (!entryOfGraph.getValue().getName().equals(NOTSET) || entryOfGraph.getValue().getNeighbors().size() > 1) {
 				Vertex currentVertex = entryOfGraph.getValue();
-				importantVertexs.put(entryOfGraph.getKey(), currentVertex);
 				List<Neighbor> newNeighbors = new ArrayList<Neighbor>();
 				for (Neighbor neighborVertex : currentVertex.getNeighbors()) {
-					reinitVisitedVertexes();
+					visitedVertexes.clear();
 					Vertex nextVertex = graph.get(neighborVertex.getName());
-					int dis = neighborVertex.getDis();
-					while (nextVertex.getName().equals("null") && nextVertex.getNeighbors().size() == 1 && !visitedVertexes.contains(nextVertex)) {
-						addToVisitedVertexes(nextVertex);
+					int distance = neighborVertex.getDistance();
+					while (nextVertex.getName().equals(NOTSET) && nextVertex.getNeighbors().size() == 1 && !visitedVertexes.contains(nextVertex)) {
+						visitedVertexes.add(nextVertex);
 						neighborVertex = nextVertex.getNeighbors().get(0);
-						dis += neighborVertex.getDis();
+						distance += neighborVertex.getDistance();
 						nextVertex = graph.get(neighborVertex.getName());
 					}
-					newNeighbors.add(new Neighbor(neighborVertex.getName(), dis));
+					newNeighbors.add(new Neighbor(neighborVertex.getName(), distance));
 				}
 				currentVertex.setNeighbors(newNeighbors);
 			}
-			if (!entryOfGraph.getValue().getName().equals("null") || entryOfGraph.getValue().getNeighbors().size() == 0) {
-				Vertex v = entryOfGraph.getValue();
-				importantVertexs.put(entryOfGraph.getKey(), v);
-			}
-
 		}
-		System.out.println("done");
 	}
 
-	private static List<Vertex> visitedVertexes = new ArrayList<Vertex>();
-	private static void reinitVisitedVertexes(){
-		visitedVertexes.clear();
-	}
-	private static void addToVisitedVertexes(Vertex vertex){
-		visitedVertexes.add(vertex);
+	public static void connectMotorwayRampsWithSameNames(TreeMap<Long, Vertex> graph){
+		
+		for (Entry<String, List<Long>> entry: MotorwayRamp.getMotorwayRamps(graph).entrySet()){
+			for(Long id:entry.getValue()){
+				Vertex actual = graph.get(id);
+				for(Long idNewN:entry.getValue()){
+					if (idNewN != id){
+						if(DistanceCalc.distanceBetweenTwoVertexs(graph.get(idNewN),graph.get(id))<600){
+						actual.addNeighbor(idNewN);
+						}
+					}
+				}
+			}
+		}
 	}
 }
