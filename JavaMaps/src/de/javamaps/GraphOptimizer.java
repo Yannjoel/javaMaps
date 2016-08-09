34a7e84612385fdb6e497ea75ef0dba80db466b1
Yannjoel <yannj96@googlemail.com>
2016-08-09 18:53:44 +0200

    Erstellung JavaDoc

--- JavaMaps/src/de/javamaps/GraphOptimizer.java
+++ JavaMaps/src/de/javamaps/GraphOptimizer.java
@@ -10,18 +10,21 @@ import de.javamaps.items.Vertex;
 
 public class GraphOptimizer {
 
-	private static final String NOTSET =  "null";
+	private static final String NOTSET = "null";
 	private static List<Vertex> visitedVertexes = new LinkedList<Vertex>();
+
 	public static void uniteVertexs(TreeMap<Long, Vertex> graph) {
-		for (Entry<Long, Vertex> entryOfGraph : graph.entrySet()){
-			if (!entryOfGraph.getValue().getName().equals(NOTSET) || entryOfGraph.getValue().getNeighbors().size() > 1) {
+		for (Entry<Long, Vertex> entryOfGraph : graph.entrySet()) {
+			if (!entryOfGraph.getValue().getName().equals(NOTSET)
+					|| entryOfGraph.getValue().getNeighbors().size() > 1) {
 				Vertex currentVertex = entryOfGraph.getValue();
 				List<Neighbor> newNeighbors = new ArrayList<Neighbor>();
 				for (Neighbor neighborVertex : currentVertex.getNeighbors()) {
 					visitedVertexes.clear();
 					Vertex nextVertex = graph.get(neighborVertex.getName());
 					int distance = neighborVertex.getDistance();
-					while (nextVertex.getName().equals(NOTSET) && nextVertex.getNeighbors().size() == 1 && !visitedVertexes.contains(nextVertex)) {
+					while (nextVertex.getName().equals(NOTSET) && nextVertex.getNeighbors().size() == 1
+							&& !visitedVertexes.contains(nextVertex)) {
 						visitedVertexes.add(nextVertex);
 						neighborVertex = nextVertex.getNeighbors().get(0);
 						distance += neighborVertex.getDistance();
@@ -34,15 +37,18 @@ public class GraphOptimizer {
 		}
 	}
 
-	public static void connectMotorwayRampsWithSameNames(TreeMap<Long, Vertex> graph){
-		
-		for (Entry<String, List<Long>> entry: MotorwayRamp.getMotorwayRamps(graph).entrySet()){
-			for(Long id:entry.getValue()){
-				Vertex actual = graph.get(id);
-				for(Long idNewN:entry.getValue()){
-					if (idNewN != id){
-						if(DistanceCalc.distanceBetweenTwoVertexs(graph.get(idNewN),graph.get(id))<600){
-						actual.addNeighbor(idNewN);
+	/**
+	 * Connect all motorwayramps with the same name inside of a Graph
+	 */
+	public static void connectMotorwayRampsWithSameNames(TreeMap<Long, Vertex> graph) {
+
+		for (Entry<String, List<Long>> entrySetOfAllMotorwayRamps : MotorwayRamp.getMotorwayRamps(graph).entrySet()) {
+			for (Long currentID : entrySetOfAllMotorwayRamps.getValue()) {
+				Vertex actualMotorwayRampAsVertex = graph.get(currentID);
+				for (Long newNeighborID : entrySetOfAllMotorwayRamps.getValue()) {
+					if (newNeighborID != currentID) {
+						if (DistanceCalc.distanceBetweenTwoVertexs(graph.get(newNeighborID), graph.get(currentID)) < 600) {
+							actualMotorwayRampAsVertex.addNeighbor(newNeighborID);
 						}
 					}
 				}
